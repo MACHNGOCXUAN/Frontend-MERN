eb643922293fa5f1ac1d6029d5d50d54cfd00dfc
Mach Ngoc Xuan <machngocxuan2004@gmail.com>
2024-07-14 10:18:41 +0700

    hoan thanh header appbar trello

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,16 +1,102 @@
 import Box from '@mui/material/Box'
 import ModeSelect from '~/components/ModeSelect'
+import IconMenu from '@mui/icons-material/AppsOutlined'
+import SvgIcon from '@mui/material/SvgIcon'
+import TrelloIcon from '~/assets/trelloIcon.svg?react'
+import Typography from '@mui/material/Typography'
+import Workspace from './Menu/Workspace'
+import Recent from './Menu/Recent'
+import Starred from './Menu/Starred'
+import Templates from './Menu/Templates'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Badge from '@mui/material/Badge'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import Tooltip from '@mui/material/Tooltip'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import Accout from './Menu/Profiles'
+import SearchIcon from '@mui/icons-material/Search'
+import InputAdornment from '@mui/material/InputAdornment'
+import { useState } from 'react'
+
 
 function AppBar () {
+
+  const [focused, setFocused] = useState(false)
+
   return (
-    <Box sx={{
-      backgroundColor:'primary.light',
+    <Box px={3} sx={{
       width:'100%',
       height: (theme ) => theme.trelloCustom.headerHeight,
       display:'flex',
-      alignItems:'center'
+      alignItems:'center',
+      justifyContent: 'space-between'
     }}>
-      <ModeSelect/>
+      <Box sx={{
+        display: 'flex',
+        flex: 1,
+        alignItems: 'center',
+        gap: 1.5,
+        color: 'primary.main'
+      }}>
+        <IconMenu fontSize='medium'/>
+        <Box sx={{
+          display: 'flex',
+          alignItems:'center',
+          gap: 1
+        }}>
+          <SvgIcon component={TrelloIcon} inheritViewBox/>
+          <Typography sx={{
+            fontWeight: 'bold',
+            fontSize: '1.1rem'
+          }}>Trello</Typography>
+        </Box>
+        <Workspace/>
+        <Recent/>
+        <Starred/>
+        <Templates/>
+        <Button variant="outlined">Create</Button>
+      </Box>
+      <Box sx={{
+        display: 'flex',
+        alignItems:'center',
+        justifyContent: 'end',
+        flex: 1,
+        gap: 2
+      }}>
+        <TextField
+          id="search"
+          placeholder='Search'
+          type="search"
+          variant="outlined"
+          size="small"
+          sx={{
+            width: focused ? '100%' : 'auto',
+            transition: 'width 0.3s ease-in-out'
+          }}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon sx={{
+                  color: 'primary.main'
+                }}/>
+              </InputAdornment>
+            )
+          }}
+        />
+        <ModeSelect/>
+        <Tooltip title="Thong bao">
+          <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }}>
+            <NotificationsNoneIcon sx={{ color: 'primary.main' }}/>
+          </Badge>
+        </Tooltip>
+        <Tooltip title="Help" sx={{ cursor: 'pointer' }}>
+          <HelpOutlineIcon sx={{ color: 'primary.main' }}/>
+        </Tooltip>
+        <Accout/>
+      </Box>
     </Box>
   )
 }
--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,10 @@
 import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-// import { red } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
   trelloCustom: {
-    headerHeight: '50px',
+    headerHeight: '58px',
     navHeight: '60px'
   },
   colorSchemes: {
@@ -19,6 +18,36 @@ const theme = extendTheme({
       palette: {
         primary:cyan,
         secondary:orange
+      }
+    }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color : theme.palette.primary.main,
+          fontSize: '0.875rem',
+          borderRadius: '10px',
+          '.MuiOutlinedInput-notchedOutline':{
+            borderColor: theme.palette.primary.light
+          },
+          '&:hover': {
+            '.MuiOutlinedInput-notchedOutline':{
+              borderColor: theme.palette.primary.light
+            }
+          },
+          // Forcus khong bi an border
+          '& fieldset':{
+            borderWidth: '1px !important'
+          }
+        })
       }
     }
   }
